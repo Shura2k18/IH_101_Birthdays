631b6e30fe5cd5db2aed02ca8b47bd8bcdca8523
Shura2k18 <43677094+Shura2k18@users.noreply.github.com>
2022-03-13 15:13:12 +0200

    Init38

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ bot.on('message', (msg) => {
             setInterval(() => {  
                 let now = new Date().getTime();
                 let t = endDate - now;
-                if (t < 0) {
+                //if (t < 0) {
                     //bot.sendPhoto(chatId, img, {caption: `\u2764\ufe0f \u2764\ufe0f \u2764\ufe0f \nЛюбі друзі, у цей чудовий день хотілось би привітати, ${add === 0 ? whom[0] : add === 1 ? whom[1] : whom[2]}, ${name} з днем народження та побажати йому щастя, здоров'я, успіхів у житті й мирного неба над головою\ud83c\uddfa\ud83c\udde6))) \nЗі святом!!!!!!!\ud83c\udf89 \ud83c\udf89 \ud83c\udf89`});
                     //bot.sendPhoto(chatId, img, {caption: `\u2764\ufe0f \u2764\ufe0f \u2764\ufe0f \nЛюбі друзі, у цей чудовий день хотілось би привітати, з днем народження та побажати йому щастя, здоров'я, успіхів у житті й мирного неба над головою\ud83c\uddfa\ud83c\udde6))) \nЗі святом!!!!!!!\ud83c\udf89 \ud83c\udf89 \ud83c\udf89`});
                     bot.sendMessage(chatId, 'ddd');
@@ -55,7 +55,9 @@ bot.on('message', (msg) => {
                     endDate = new Date(year, 02, 13, 08).getTime();
                     bot.sendMessage(chatId, year);
                     bot.sendMessage(chatId, endDate);
-                }
+                    bot.sendMessage(chatId, now);
+                    bot.sendMessage(chatId, t);
+                //}
             }, 1000);
         //});
         //bot.sendPhoto(chatId, img, {caption: `\u2764\ufe0f \u2764\ufe0f \u2764\ufe0f \nЛюбі друзі, у цей чудовий день хотілось би привітати, з днем народження та побажати йому щастя, здоров'я, успіхів у житті й мирного неба над головою\ud83c\uddfa\ud83c\udde6))) \nЗі святом!!!!!!!\ud83c\udf89 \ud83c\udf89 \ud83c\udf89`});
