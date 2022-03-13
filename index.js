6a9a12b39c0d8fe51770daece0f54ed4ae307761
Shura2k18 <43677094+Shura2k18@users.noreply.github.com>
2022-03-13 21:19:38 +0200

    Init62

--- index.js
+++ index.js
@@ -10,27 +10,25 @@ const bot = new TelegramBot(process.env.TOKEN, {polling: true});
 const intervalfunc = (chatId, month, day, whom, name, add) => {
     let year = new Date().getFullYear();
     const time = new Date().getTime();
-    const timed = new Date(year, month, day, 20, 00).getTime();
+    const timed = new Date(year, month, day, 22, 00).getTime();
     let endDate;
-    if(timed - time < 0) {
+    if(timed < time) {
         year += 1;
         endDate = new Date(year, month, day, 08, 00).getTime();   
     } else {
-        endDate = new Date(year, month, day, 19, 57).getTime();
+        endDate = new Date(year, month, day, 21, 22).getTime();
     }
     //let t = endDate - time;
     //bot.sendMessage(chatId, `${t}   a`)
     const timer = setInterval(function() {  
         let now = new Date().getTime();
-        let t = endDate - now;
-        if (t <= 0) {
-            bot.sendPhoto(chatId, img, {caption: `\u2764\ufe0f \u2764\ufe0f \u2764\ufe0f \nЛюбі друзі, у цей чудовий день хотілось би привітати, ${add === 0 ? whom[0] : add === 1 ? whom[1] : whom[2]}, ${name} з днем народження та побажати йому щастя, здоров'я, успіхів у житті й мирного неба над головою\ud83c\uddfa\ud83c\udde6))) \nЗі святом!!!!!!!\ud83c\udf89 \ud83c\udf89 \ud83c\udf89`});
+        //let t = endDate - now;
+        if (now > endDate) {
+            //bot.sendPhoto(chatId, img, {caption: `\u2764\ufe0f \u2764\ufe0f \u2764\ufe0f \nЛюбі друзі, у цей чудовий день хотілось би привітати, ${add === 0 ? whom[0] : add === 1 ? whom[1] : whom[2]}, ${name} з днем народження та побажати йому щастя, здоров'я, успіхів у житті й мирного неба над головою\ud83c\uddfa\ud83c\udde6))) \nЗі святом!!!!!!!\ud83c\udf89 \ud83c\udf89 \ud83c\udf89`});
             bot.sendMessage(chatId, 'aaa')
             year += 1;
             endDate = new Date(year, month, day, 08).getTime();
             return 0;
-        } else {
-            bot.sendMessage(chatId, 'bbb')
         }
     }, 1000);
 }
