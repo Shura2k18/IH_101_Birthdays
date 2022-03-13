1131f6137a875e5c6acc26f32a16e905c9bb4ca9
Shura2k18 <43677094+Shura2k18@users.noreply.github.com>
2022-03-13 19:44:40 +0200

    Init57

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ const intervalfunc = (chatId, month, day, whom, name, add) => {
         year += 1;
         endDate = new Date(year, month, day, 08, 00).getTime();   
     } else {
-        endDate = new Date(year, month, day, 20, 00).getTime();
+        endDate = new Date(year, month, day, 19, 48).getTime();
     }
-    let t = endDate - time;
-    bot.sendMessage(chatId, `${t}   a`)
+    //let t = endDate - time;
+    //bot.sendMessage(chatId, `${t}   a`)
     const timer = setInterval(function() {  
-        // let now = new Date().getTime();
-        // let t = now - endDate;
-        // if (t <= 0) {
-        //     bot.sendPhoto(chatId, img, {caption: `\u2764\ufe0f \u2764\ufe0f \u2764\ufe0f \nЛюбі друзі, у цей чудовий день хотілось би привітати, ${add === 0 ? whom[0] : add === 1 ? whom[1] : whom[2]}, ${name} з днем народження та побажати йому щастя, здоров'я, успіхів у житті й мирного неба над головою\ud83c\uddfa\ud83c\udde6))) \nЗі святом!!!!!!!\ud83c\udf89 \ud83c\udf89 \ud83c\udf89`});
-        //     year += 1;
-        //     endDate = new Date(year, month, day, 08).getTime();
-        //     return 0;
-        // }
+        let now = new Date().getTime();
+        let t = now - endDate;
+        if (t <= 0) {
+            bot.sendPhoto(chatId, img, {caption: `\u2764\ufe0f \u2764\ufe0f \u2764\ufe0f \nЛюбі друзі, у цей чудовий день хотілось би привітати, ${add === 0 ? whom[0] : add === 1 ? whom[1] : whom[2]}, ${name} з днем народження та побажати йому щастя, здоров'я, успіхів у житті й мирного неба над головою\ud83c\uddfa\ud83c\udde6))) \nЗі святом!!!!!!!\ud83c\udf89 \ud83c\udf89 \ud83c\udf89`});
+            year += 1;
+            endDate = new Date(year, month, day, 08).getTime();
+            return 0;
+        }
     }, 1000);
 }
 bot.onText(/start(.+)/, (msg) => {
