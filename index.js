ad3227d0292bcbab833c14713b4b3c29a0f8ac7b
Shura2k18 <43677094+Shura2k18@users.noreply.github.com>
2022-03-13 16:35:18 +0200

    Init47

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const intervalfunc = (chatId, month, day, whom, name, add) => {
         let t = endDate - now;
         bot.sendMessage(chatId, `${now}a`)
         bot.sendMessage(chatId, `${t}b`)
+        return 0
         // if (t < 0) {
         //     bot.sendPhoto(chatId, img, {caption: `\u2764\ufe0f \u2764\ufe0f \u2764\ufe0f \nЛюбі друзі, у цей чудовий день хотілось би привітати, ${add === 0 ? whom[0] : add === 1 ? whom[1] : whom[2]}, ${name} з днем народження та побажати йому щастя, здоров'я, успіхів у житті й мирного неба над головою\ud83c\uddfa\ud83c\udde6))) \nЗі святом!!!!!!!\ud83c\udf89 \ud83c\udf89 \ud83c\udf89`});
         //     year += 1;
